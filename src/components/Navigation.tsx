88b2116ba8a900e320cf273aedef1adceac51f81
alperenozdnc <alperenozdincc@gmail.com>
2025-05-18 18:46:04 +0300

    feat: navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 
+import "./Navigation.css";
+
 export const Navigation: React.FC = () => {
     return (
         <header>
-            <nav>Navigation bar</nav>
+            <nav className="nav">
+                <div className="nav-container">
+                    <ul className="nav-list">
+                        <li className="nav-item">
+                            <a href="/layouts/" className="nav-link">
+                                Layouts
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/stats/" className="nav-link">
+                                Stats
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/modify/" className="nav-link">
+                                Modify
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     );
 };
