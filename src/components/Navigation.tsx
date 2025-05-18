7955bf1a9e7d9c05a7b7a4fd7ba46fc5ceac9201
alperenozdnc <alperenozdincc@gmail.com>
2025-05-18 19:11:49 +0300

    refactor: replace `a` with react router `Link`

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import "./Navigation.css";
 
 export const Navigation: React.FC = () => {
@@ -9,24 +11,24 @@ export const Navigation: React.FC = () => {
                 <div className="nav-container">
                     <ul className="nav-list">
                         <li className="nav-item">
-                            <a href="/" className="nav-link">
+                            <Link to="/" className="nav-link">
                                 Home
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a href="/layouts/" className="nav-link">
+                            <Link to="/layouts/" className="nav-link">
                                 Layouts
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a href="/stats/" className="nav-link">
+                            <Link to="/stats/" className="nav-link">
                                 Stats
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a href="/modify/" className="nav-link">
+                            <Link to="/modify/" className="nav-link">
                                 Modify
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
