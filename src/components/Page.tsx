5be954734be9eb0f656324d3a8761eabbc663af0
alperenozdnc <alperenozdincc@gmail.com>
2025-05-28 19:08:13 +0300

    feat: fix nav to top on scroll

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,15 +1,17 @@
 import React, { type JSX } from "react";
 import { Navigation } from "./Navigation";
 
+import "./Page.css";
+
 interface Props {
     element: JSX.Element;
 }
 
 export const Page: React.FC<Props> = ({ element }) => {
     return (
-        <>
+        <div className="page-container">
             <Navigation />
             {element}
-        </>
+        </div>
     );
 };
